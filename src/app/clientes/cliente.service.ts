ff1e0999ff8bf3ee31bca2802e8557d0b4332252
josegazano732 <josegazano7@outlook.com>
2022-06-22 08:15:53 -0300

    Se trabajo con manejo de errores en metodo GET en servicio JLG

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
 import { Cliente } from './cliente';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import Swal from 'sweetalert2';
+
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +16,7 @@ export class ClienteService {
 
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router:Router) { }
 
   getClientes():Observable <Cliente[]> {
     //return of(CLIENTES);
@@ -27,7 +30,17 @@ export class ClienteService {
   }
 
   getCliente(id): Observable <Cliente>{
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes'])
+        //console.error(e.error.mensaje)
+        console.log(e.error.mensaje);
+        
+        Swal.fire('Error al editar', e.error.mensaje, 'error');
+        //return throwError(e);
+        return throwError( () => e );
+      })
+    )
   }
 
   update(cliente:Cliente): Observable <Cliente>{
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -47,6 +47,8 @@ export class FormComponent implements OnInit {
     })
   }
 
+  
+
 
 
 }
