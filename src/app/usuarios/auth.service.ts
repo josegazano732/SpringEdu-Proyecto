da439d0893dc75c66142a5db4a35c562426d31e1
JLG <josegazano7@outlook.com>
2022-07-31 15:07:27 -0300

    Se implemento la clase de servicio AuthService y su metodo. JLG

---
+++ src/app/usuarios/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from './usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(usuario:Usuario):Observable<any>{
+    const urlEndPoint = 'http://localhost:8080/oauth/token';
+
+    const credenciales = btoa('angularapp' + ':' + '12345');
+
+    const httpHeaders = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded',
+  'Authorization': 'Basic ' + credenciales});
+
+    let params = new URLSearchParams();
+    params.set('grant_type', 'password');
+    params.set('username', usuario.username);
+    params.set('password', usuario.password);
+    
+    return this.http.post(urlEndPoint, params, {headers: httpHeaders});
+  }
+}
