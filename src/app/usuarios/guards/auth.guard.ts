633467fc9ea88d12ff125c865c7883653b69105a
JLG <josegazano7@outlook.com>
2022-08-07 20:54:18 -0300

    Se implementa validando fechas de expiracion del token el AuthGuard. JLG

--- src/app/usuarios/guards/auth.guard.ts
+++ src/app/usuarios/guards/auth.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { now } from 'moment';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
 
@@ -15,10 +16,25 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       if(this.authService.isAuthenticated()){
+        if(this.isTokenExpirado()){
+          this.authService.logout();
+          this.router.navigate(['/login']);
+          return  false;
+        }
         return true;
       }
       this.router.navigate(['/login']);
     return false;
   }
-  
+  //Metodo para verificar si token se encuentra expirado validando las fechas.
+  isTokenExpirado():boolean{
+    let token = this.authService.token;
+    let payload = this.authService.obtenerDatosToken(token);
+    let now = new Date().getTime() /1000;
+    if(payload.exp < now){
+      return true;
+    }
+    return false;
+
+}
 }
