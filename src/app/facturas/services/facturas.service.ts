ee00222b9a0186e80af544ad8b8e8946e58a4aab
JLG <josegazano7@outlook.com>
2022-08-19 12:30:24 -0300

    Se agrego metodo detFactura en Service de factura. JLG

--- src/app/facturas/services/facturas.service.ts
+++ src/app/facturas/services/facturas.service.ts
@@ -1,3 +1,6 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Factura } from '../models/factura';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +8,18 @@ import { Injectable } from '@angular/core';
 })
 export class FacturasService {
 
-  constructor() { }
+  private urlEndPoint: string = 'http://localhost:8080/api/facturas';
+
+  constructor(private http: HttpClient) { }
+
+  getFactura(id:number):Observable<Factura>{
+    //1ª Forma
+    //return this.http.get<Factura>(this.urlEndPoint+'/'+id);
+    //2ª Forma:
+    return this.http.get<Factura>(`${this.urlEndPoint}/${id}`);
+    
+
+  }
+
+ 
 }
