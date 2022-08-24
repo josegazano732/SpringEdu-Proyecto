800435608d39ded4b669e5540d57d52514f50964
JLG <josegazano7@outlook.com>
2022-08-24 13:33:13 -0300

    Metodos facturaComponent: existeItem y actualizarCantidad. JLG

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -57,21 +57,49 @@ export class FacturasComponent implements OnInit {
     let producto = event.option.value as Producto;
     console.log(producto);
 
-    let nuevoItem = new ItemFactura();
-    nuevoItem.producto = producto;
-    this.factura.items.push(nuevoItem);
+    if(this.existeItem(producto.id)){
+      this.incrementaCantidad(producto.id);
+    } else{
+      let nuevoItem = new ItemFactura();
+      nuevoItem.producto = producto;
+      this.factura.items.push(nuevoItem);
+    }
+
+    
 
     this.autocompleteControl.setValue(' ');
     event.option.focus();
     event.option.deselect();
   }
 
+  //Metodo para actualizar cantidad la cantidad de producto.
   actualizarCantidad(id:number, event:any): void{
+    //Obtengo cant. de input
     let cantidad:number = event.target.value as number;
 
     this.factura.items = this.factura.items.map((item:ItemFactura) => {
       if(id === item.producto.id){
         item.cantidad = cantidad;
+      }
+      return item;
+    });
+  }
+
+  existeItem(id:number):boolean{
+    let existe = false;
+    this.factura.items.forEach((item:ItemFactura) =>{
+      if(id === item.producto.id){
+        existe = true;
+      } 
+    })
+    return existe;
+  }
+
+  incrementaCantidad(id:number):void{
+
+    this.factura.items = this.factura.items.map((item:ItemFactura) => {
+      if(id === item.producto.id){
+        item.cantidad = item.cantidad + 1;
       }
       return item;
     });
